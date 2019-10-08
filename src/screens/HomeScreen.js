3fed114e64dbb87631da2ed5272a4cd40d6ee276
Michael Duberstein <mduberst@amazon.com>
2019-10-08 11:11:40 -0400

    After Clip 29, After adding ImageScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = (props) => {
-  // console.log(props.navigation);
+
+const HomeScreen = ({navigation}) => {
   return <View>
       <Text style={styles.text}>Hi There!</Text>
       <Button
       onPress={()=>
-        // console.log('Button pressed'
-        props.navigation.navigate('Components')} 
+        navigation.navigate('Components')} 
       title="Go to Components Demo"/> 
       <Button onPress={()=>
-        props.navigation.navigate('List')} 
+        navigation.navigate('List')} 
         title='Go To List Demo'/>
-      {/* <TouchableOpacity onPress={()=>
-        // console.log('List pressed')
-        props.navigation.navigate('List')} >
-        <Text>Go To List Demo</Text>
-        <Text>Go To List Demo</Text>
-        <Text>Go To List Demo</Text>
-      </TouchableOpacity>   */}
+      <Button onPress={()=>
+        navigation.navigate('Image')} 
+        title='Go To Image Demo'/>
     </View>
 };
+
+// Before Clip 27
+// const HomeScreen = (props) => {
+//   // console.log(props.navigation);
+//   return <View>
+//       <Text style={styles.text}>Hi There!</Text>
+//       <Button
+//       onPress={()=>
+//         // console.log('Button pressed'
+//         props.navigation.navigate('Components')} 
+//       title="Go to Components Demo"/> 
+//       <Button onPress={()=>
+//         props.navigation.navigate('List')} 
+//         title='Go To List Demo'/>
+//       {/* <TouchableOpacity onPress={()=>
+//         // console.log('List pressed')
+//         props.navigation.navigate('List')} >
+//         <Text>Go To List Demo</Text> 
+//         <Text>Go To List Demo</Text>
+//         <Text>Go To List Demo</Text>
+//       </TouchableOpacity>   */}
+//     </View>
+// };
 
 const styles = StyleSheet.create({
   text: {
